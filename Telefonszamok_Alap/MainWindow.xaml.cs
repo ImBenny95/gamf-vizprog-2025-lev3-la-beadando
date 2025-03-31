3c1961db30b74999de44e99af83be0d7546b1ba3
Tóth László <lastoth@gmail.com>
2025-03-31 09:10:03 +0200

    Add project files.

---
+++ Telefonszamok_Alap/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using cnTelefonkonyv;
+
+namespace Telefonszamok_Alap;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    TelefonkonyvContext _context;
+    public MainWindow()
+    {
+        InitializeComponent();
+        _context = new TelefonkonyvContext();
+        
+    }
+
+
+
+}
